8c24227520c148160c27907a8869bc1ab421bb85
Brian Bleck <bbleck@unm.edu>
2019-03-28 11:06:13 -0600

    Initial Commit

---
+++ test2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test2
+//
+//  Created by Brian on 3/28/19.
+//  Copyright © 2019 Brian Bleck. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
